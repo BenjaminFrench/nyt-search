4196a53729baa53db873408ed06a0c8f8b850766
Ben French <ben.jminw@gmail.com>
2017-12-09 13:21:26 -0700

    more js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,18 +2,39 @@
 
 $('#searchBtn').on('click', function (event) {
     event.preventDefault();
-    var searchTerm = $('#searchTerm').val()
+    var searchTerm = $('#searchTerm').val();
+    var numResults = $('#numberRecords').val();
+    var startDate = $('#startYear').val();
+    var endDate = $('#endYear').val();
     
     var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     url += '?' + $.param({
         'api-key': "b9f91d369ff59547cd47b931d8cbc56b:0:74623931",
-        'q': "trump"
+        'q': searchTerm,
+        'begin_date': startDate,
+        'end_date': endDate
     });
     $.ajax({
         url: url,
         method: 'GET',
     }).done(function(result) {
-        console.log(result);
+        response = result.response;
+        console.log(response);
+
+        for (let index = 0; index < numResults; index++) {
+            $('#queryResults').append(`
+            <div class="card">
+                <div class="body">
+                    <h3 class="headline"><a href="${response.docs[index].web_url}"> Article number: ${index+1} ${response.docs[index].headline.main}</a></h3>
+                    <h5 class="author">${response.docs[index].byline.original}</h5>
+                </div>
+            </div>
+            `)
+        }
+        // response.docs.array.forEach(element => {
+            
+        // });
+
     }).fail(function(err) {
         throw err;
     });
